d78c114d70240302b3ea08d6856bb53a8f4ee919
初时y <2283873481@qq.com>
2024-04-28 13:48:01 +0800

    feat(add): 新增Concumer消费函数式接口

---
+++ chushiy-standard/src/main/java/com/chushiy/standard/function/Consumer.java
@@ -0,0 +1,18 @@
+package com.chushiy.standard.function;
+
+/**
+ * @Author 初时y
+ * @Email 2283873481@qq.com
+ * @DateTime 2024/4/28 下午 1:44
+ * @Description 消费函数接口
+ * @ProjectName chushiy-framework
+ * @PackageName com.chushiy.standard.function
+ * @ClassName Consumer.java
+ * @ProductName IntelliJ IDEA
+ * @Version 1.0
+ */
+@FunctionalInterface
+public interface Consumer {
+
+    void accept();
+}
