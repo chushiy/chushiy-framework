ecf933a0c497850436583faec6a76fcfc8e9221f
初时y <2283873481@qq.com>
2025-04-04 13:48:50 +0800

    feat(standard): 添加 chushiy 工具类
    - 新增 ChuShiyUtils 工具类，用于验证值是否正确
    - 添加 CHUSHIY 常量，用于存储验证值
    - 实现 verify 方法，验证传入值是否与 CHUSHIY常量相等

---
+++ chushiy-standard/src/main/java/com/chushiy/standard/util/ChuShiyUtils.java
@@ -0,0 +1,29 @@
+package com.chushiy.standard.util;
+
+import cn.hutool.core.lang.Assert;
+
+/**
+ * @Author 初时y
+ * @Email chushiy@qq.com
+ * @DateTime 2025/4/4 13:45
+ * @Description chushiy工具类
+ * @ProjectName chushiy-framework
+ * @PackageName com.chushiy.standard.util
+ * @ClassName ChuShiyUtils.java
+ * @ProductName IntelliJ IDEA
+ * @Version 1.0.0
+ */
+public class ChuShiyUtils {
+
+    public static final String CHUSHIY = "chushiy";
+
+    /**
+     * 验证值是否正确
+     *
+     * @param val 验证值
+     * @throws IllegalArgumentException exception
+     */
+    public void verify(String val) throws IllegalArgumentException {
+        Assert.isTrue(CHUSHIY.equals(val));
+    }
+}
