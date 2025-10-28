8bf0e31f0bd697d5839691d7e1a47b6f606bcf5d
初时y <2283873481@qq.com>
2025-10-28 16:27:27 +0800

    refactor(ip):重构IP地址定位工具类
    - 使用Hutool工具类加载IP地址库文件
    - 简化初始化逻辑，移除文件路径拼接相关代码
    -优化异常处理，使用自定义业务异常替代原生异常- 清理无用代码和注释，提高代码可读性
    - 调整变量命名，提升代码规范性
    - 改进IP查询结果处理逻辑，去除无效字符
    - 更新日志记录方式，增强错误提示信息
    
    (cherry picked from commit 7075539d5e742ff4b36253d0d0aa355518b0c61d)

--- chushiy-standard/src/main/java/com/chushiy/standard/ip/Ip2regionUtils.java
+++ chushiy-standard/src/main/java/com/chushiy/standard/ip/Ip2regionUtils.java
@@ -1,94 +1,64 @@
-// package com.chushiy.standard.ip;
-//
-// import lombok.extern.slf4j.Slf4j;
-// import org.lionsoul.ip2region.xdb.Searcher;
-//
-// import java.io.File;
-// import java.io.InputStream;
-// import java.util.concurrent.TimeUnit;
-//
-// /**
-//  * @Author 初时y
-//  * @Email chushiy@qq.com
-//  * @DateTime 2025/9/8 18:29
-//  * @Description 离线IP地址定位库和IP定位数据
-//  * @ProjectName chushiy-framework
-//  * @PackageName com.chushiy.standard.ip
-//  * @ClassName Ip2regionUtils.java
-//  * @ProductName IntelliJ IDEA
-//  * @Version 1.0.0
-//  */
-// @Slf4j
-// public class Ip2regionUtils {
-//
-//     private static Searcher searcher;
-//
-//     private static String IP2REGION_DB_PATH;
-//
-//     static {
-//         try {
-//             getIp2regionDbPath();
-//             init();
-//         } catch (Exception e) {
-//             throw new RuntimeException(e);
-//         }
-//     }
-//
-//     private static void getIp2regionDbPath() throws Exception {
-//         // 获取当前项目所在的绝对路径
-//         String proFilePath = System.getProperty("user.dir");
-//
-//         String ip2regionFilePath = "/ip2region";
-//         String ip2regionDbFileName = "ip2region.xdb";
-//
-//         // 获取模板下的路径，然后存放在temp目录下　
-//         String newFilePath = proFilePath + File.separator + "temp" + File.separator + ip2regionFilePath;
-//         newFilePath = newFilePath.replace("/", File.separator);
-//         // 检查项目运行时的src下的对应路径
-//         File newFile = new File(newFilePath + ip2regionDbFileName);
-//         if (newFile.isFile() && newFile.exists()) {
-//             return;
-//         }
-//         // 当项目打成jar包会运行下面的代码，并且复制一份到src路径下
-//         InputStream certStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(ip2regionFilePath + ip2regionDbFileName);
-//         byte[] certData = org.apache.commons.io.IOUtils.toByteArray(certStream);
-//         org.apache.commons.io.FileUtils.writeByteArrayToFile(newFile, certData);
-//
-//         IP2REGION_DB_PATH = newFile.getAbsolutePath();
-//     }
-//
-//     private static void init() throws Exception {
-//         // 从 dbPath 加载整个 xdb 到内存。
-//         byte[] cBuff;
-//         try {
-//             cBuff = Searcher.loadContentFromFile(Ip2regionUtils.class.getResource(IP2REGION_DB_PATH).getPath());
-//         } catch (Exception e) {
-//             log.error("failed to load content from {}", Ip2regionUtils.class.getResource(IP2REGION_DB_PATH).getPath(), e);
-//             return;
-//         }
-//
-//         // 使用上述的 cBuff 创建一个完全基于内存的查询对象。
-//         try {
-//             searcher = Searcher.newWithBuffer(cBuff);
-//         } catch (Exception e) {
-//             log.error("failed to create content cached searcher: ", e);
-//             throw e;
-//         }
-//     }
-//
-//     public static String getIp2Region(String ip) {
-//         try {
-//             long sTime = System.nanoTime();
-//             String region = searcher.search(ip);
-//             long cost = TimeUnit.NANOSECONDS.toMicros((long) (System.nanoTime() - sTime));
-//             log.info("{region: {}, ioCount: {}, took: {} μs}\n", region, searcher.getIOCount(), cost);
-//             return region;
-//         } catch (Exception e) {
-//             return null;
-//         }
-//     }
-//
-//     public static void main(String[] args) {
-//         System.out.println(getIp2Region("219.136.76.91"));
-//     }
-// }
+package com.chushiy.standard.ip;
+
+import cn.hutool.core.io.resource.NoResourceException;
+import cn.hutool.core.io.resource.ResourceUtil;
+import com.chushiy.standard.exception.BizException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.lionsoul.ip2region.xdb.Searcher;
+
+/**
+ * @Author 初时y
+ * @Email chushiy@qq.com
+ * @DateTime 2025/9/8 18:29
+ * @Description 离线IP地址定位库和IP定位数据
+ * @ProjectName chushiy-framework
+ * @PackageName com.chushiy.standard.ip
+ * @ClassName Ip2regionUtils.java
+ * @ProductName IntelliJ IDEA
+ * @Version 1.0.0
+ */
+@Slf4j
+public class Ip2regionUtils {
+
+    private static Searcher SEARCHER;
+
+    /**
+     * IP地址库文件路径
+     */
+    private static final String IP2REGION_DB_PATH = "ip2region/ip2region.xdb";
+
+    static {
+        try {
+            // 1、将 ip2region 数据库文件 xdb 从 ClassPath 加载到内存。
+            // 2、基于加载到内存的 xdb 数据创建一个 Searcher 查询对象。
+            SEARCHER = Searcher.newWithBuffer(ResourceUtil.readBytes(IP2REGION_DB_PATH));
+            log.info("Ip2regionUtils初始化成功，加载IP地址库数据成功！");
+        } catch (NoResourceException e) {
+            throw BizException.of("RegionUtils初始化失败，原因：IP地址库数据不存在！");
+        } catch (Exception e) {
+            throw BizException.of("Ip2regionUtils初始化失败，原因：" + e.getMessage());
+        }
+    }
+
+    /**
+     * 获取IP地址
+     *
+     * @param ip ip
+     * @return IP地址
+     */
+    public static String getIp2Region(String ip) {
+        try {
+            // 3、执行查询
+            String region = SEARCHER.search(StringUtils.trim(ip));
+            return region.replace("0|", "").replace("|0", "");
+        } catch (Exception e) {
+            log.error("IP地址离线获取城市异常 {}", ip);
+            return "未知";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getIp2Region("119.130.132.148"));
+    }
+}
